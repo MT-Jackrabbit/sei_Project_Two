e1d1f24b98cc366e14352fb581676e02b564e90b
Jeff Jackson <jj@rtdronline.com>
2020-06-18 13:00:46 -0500

    adds models and registers them

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=100)
+    imageURL = models.CharField(max_length=250)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    # will create the date and set editable=False and blank=True
+    joined_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(max_length=1000)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
